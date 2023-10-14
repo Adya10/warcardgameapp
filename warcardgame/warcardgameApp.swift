4fa835dd2cfe1e38d67992a53055cf0b4d0d6dee
Advait Lanjekar <adya.lanjekar@gmail.com>
2023-10-14 17:38:49 +0200

    Initial Commit

---
+++ warcardgame/warcardgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  warcardgameApp.swift
+//  warcardgame
+//
+//  Created by Advait Lanjekar on 14/10/23.
+//
+
+import SwiftUI
+
+@main
+struct warcardgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
